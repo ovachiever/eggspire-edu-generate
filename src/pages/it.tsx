e77b9ed67afc712965a8ea6de11120a260c49078
ovachiever <144160007+ovachiever@users.noreply.github.com>
2025-01-29 12:22:04 -0800

    Create it.tsx

---
+++ src/pages/it.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IT = () => {
+  return (
+    <div>
+      <h1>Information Technology</h1>
+      <p>Welcome to the IT department. Here, you can find resources and training modules related to IT.</p>
+    </div>
+  );
+};
+
+export default IT;
