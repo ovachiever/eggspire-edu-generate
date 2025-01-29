e11223f3b15a06bb3b70ddee492fd8c05b408285
ovachiever <144160007+ovachiever@users.noreply.github.com>
2025-01-29 12:23:19 -0800

    Create accounting.tsx

---
+++ src/pages/accounting.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Accounting = () => {
+  return (
+    <div>
+      <h1>Accounting</h1>
+      <p>Welcome to the Accounting department. Here, you can find resources and training modules related to accounting.</p>
+    </div>
+  );
+};
+
+export default Accounting;
